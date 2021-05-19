80e93e43ffa30e9cfaf3247744da3aeb7c11395f
chriscod91 <80732087+chriscod91@users.noreply.github.com>
2021-05-18 19:02:56 -0700

    Add files via upload

---
+++ Tasksmanager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace taskmaneger.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
